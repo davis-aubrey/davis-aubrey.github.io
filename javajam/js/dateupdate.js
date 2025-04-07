3d116c61536f637de948d5fe9a305f37e73787f6
Aubrey Davis <a02340854@usu.edu>
2025-04-06 23:50:10 -0600

    Assignment 9
    Adding Javascript

---
+++ javajam/js/dateupdate.js
@@ -0,0 +1,2 @@
+// Create an external script called dateupdate.js in the js folder that utilizes getElementById and innerHTML to show the date a page was last modified.
+document.getElementById("dateupdate").innerHTML = "The page was last modified on " + document.lastModified;
