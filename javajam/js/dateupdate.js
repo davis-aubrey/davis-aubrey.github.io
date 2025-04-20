5d56d89abb2e955d43ce59f84a8747939564f31e
Aubrey Davis <a02340854@usu.edu>
2025-04-19 22:12:42 -0600

    Photography website update
    Added pages and functionality. Pushing to check for fixes.

--- javajam/js/dateupdate.js
+++
@@ -1,2 +0,0 @@
-// Create an external script called dateupdate.js in the js folder that utilizes getElementById and innerHTML to show the date a page was last modified.
-document.getElementById("dateupdate").innerHTML = "The page was last modified on " + document.lastModified;
